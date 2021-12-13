1114a944babf5b024c63217b2050851c6fb97fd0
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2021-12-13 15:03:24 +0900

    Initial Setting

--- src/elements/Index.jsx
+++ src/elements/Index.jsx
@@ -6,5 +6,6 @@ import Image from '../elements/Image';
 import Input from '../elements/Input';
 import Span from '../elements/Span';
 import Text from '../elements/Text';
+import Spinner from '../elements/Spinner';
 
-export {Button, Grid, Input, Text, Span, Image};
+export {Button, Grid, Input, Text, Span, Image, Spinner};
--- src/elements/Input.jsx
+++ src/elements/Input.jsx
@@ -1,20 +1,84 @@
 // *** Input.jsx ***
 
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+
+import { Text, Grid } from "./index";
 
 const Input = (props) => {
+  const {
+    label,
+    placeholder,
+    _onChange,
+    _onKeyUp,
+    type,
+    multiLine,
+    value,
+    is_submit,
+    onSubmit,
+  } = props;
 
-    const {} = props;
-    return(
-        <React.Fragment>
+  if (multiLine) {
+    return (
+      <Grid>
+        {label && <Text margin="0px">{label}</Text>}
+        <ElTextarea
+          rows={10}
+          value={value}
+          placeholder={placeholder}
+          onChange={_onChange}
+        ></ElTextarea>
+      </Grid>
+    );
+  }
 
-        </React.Fragment>
-    )
-}
+  return (
+    <React.Fragment>
+      <Grid>
+        {label && <Text margin="0px">{label}</Text>}
+        {is_submit ? (
+          <ElInput
+            type={type}
+            placeholder={placeholder}
+            onChange={_onChange}
+            value={value}
+            onKeyPress={(e) => {
+              if (e.key === "Enter") {
+                onSubmit(e);
+              }
+            }}
+          />
+        ) : (
+          <ElInput type={type} placeholder={placeholder} onChange={_onChange} onKeyUp={_onKeyUp}/>
+        )}
+      </Grid>
+    </React.Fragment>
+  );
+};
 
 Input.defaultProps = {
+  multiLine: false,
+  label: false,
+  placeholder: "텍스트를 입력해주세요.",
+  type: "text",
+  value: "",
+  is_submit: false,
+  onSubmit: () => {},
+  _onChange: () => {},
+};
 
-}
+const ElTextarea = styled.textarea`
+  border: 1px solid #212121;
+  width: 100%;
+  padding: 12px 4px;
+  box-sizing: border-box;
+`;
+
+const ElInput = styled.input`
+  border: 1px solid #212121;
+  width: 100%;
+  padding: 12px 4px;
+  box-sizing: border-box;
+`;
 
 export default Input;
