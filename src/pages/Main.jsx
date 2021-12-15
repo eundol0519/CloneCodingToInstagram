5d3c904b5809569ca49bf5c5caacbe3bf6eb7e63
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2021-12-15 14:06:01 +0900

    merge

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,32 +1,60 @@
 // *** Main.jsx ***
 
 import React from 'react';
-import { Grid, Image, Button } from '../elements/index';
+import styled from 'styled-components';
 import PostDetail from '../pages/PostDetail';
+import Post from '../components/Post';
 
 const Main = props => {
   const [postDetailModal, setPostDetailModal] = React.useState(false);
 
   return (
     <React.Fragment>
-      <Grid padding="70px 0px 0px 0px"></Grid>
-      <Grid padding="30px 0px 0px 0px" center>
-        <button
-          onClick={() => {
-            setPostDetailModal(true);
-          }}
-        >
-          상세 페이지
-        </button>
-        {postDetailModal && (
-          <PostDetail
-            modal={postDetailModal}
-            setPostDetailModal={setPostDetailModal}
-          ></PostDetail>
-        )}
-      </Grid>
+      <button
+        onClick={() => {
+          setPostDetailModal(true);
+        }}
+      >
+        상세 페이지
+      </button>
+      {postDetailModal && (
+        <PostDetail
+          modal={postDetailModal}
+          setPostDetailModal={setPostDetailModal}
+        ></PostDetail>
+      )}
+      <Container>
+        <Item>
+          <Post />
+        </Item>
+        <Item>
+          <Post />
+        </Item>
+        <Item>
+          <Post />
+        </Item>
+        <Item>
+          <Post />
+        </Item>
+      </Container>
     </React.Fragment>
   );
 };
+
+const Container = styled.div`
+  max-width: 614px;
+  /* width: 891px; */
+  margin: 0 auto;
+  /* background-color: aqua; */
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const Item = styled.div`
+  width: 614px;
+  height: 891px;
+  /* border: 2px
+  background-color: blueviolet; */
+`;
 
 export default Main;
--- src/redux/modules/post.js
+++ src/redux/modules/post.js
@@ -5,9 +5,53 @@ import apis from '../../shared/apis';
 // Actions
 
 const ADD_POST = 'ADD_POST';
-const GET_POST = 'GET_POST';
+const GET_ONE_POST = 'GET_ONE_POST';
+const GET_POST = 'GET_POST'; // 항민
+const GET_MYPOST = 'GET_MYPOST'; // 항민
 
 const initialState = {
+  postList: [
+    {
+      postId: 1,
+      content: '카카오프렌즈',
+      likeCount: '2',
+      userNickname: '초콜렛',
+      imageUrl:
+        'https://www.google.com/imgres?imgurl=https%3A%2F%2Ft1.kakaocdn.net%2Fkakaocorp%2Fkakaocorp%2Fadmin%2Fservice%2Fa85d0594017900001.jpg&imgrefurl=https%3A%2F%2Fwww.kakaocorp.com%2Fpage%2Fdetail%2F580&tbnid=1bXXZ3LxWNcp9M&vet=12ahUKEwjFtYH4j-H0AhWVCt4KHb9ZAXsQMygAegUIARDSAQ..i&docid=1F2bVLbF5-kfJM&w=1920&h=1079&itg=1&q=%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88&ved=2ahUKEwjFtYH4j-H0AhWVCt4KHb9ZAXsQMygAegUIARDSAQ',
+      createdAt: '2021-12-13',
+    },
+  ],
+
+  post: {
+    postId: 4,
+    content: '글내용',
+    likeCount: '2',
+    userNickname: '스펀지밥',
+    imageUrl: 'uploads/posts/1639370169898_myPhoto.jpg',
+    createdAt: '2021-12-13',
+  },
+
+  myPageList: [
+    {
+      userEmail: 'test@test.com',
+      userName: '라이언',
+      nickname: '라이언일병구하기',
+      imageUrl_profile:
+        'https://www.google.com/imgres?imgurl=https%3A%2F%2Fitem.kakaocdn.net%2Fdo%2F30cef086c8778d80e1487385bd5efe7b82f3bd8c9735553d03f6f982e10ebe70&imgrefurl=https%3A%2F%2Fe.kakao.com%2Ft%2Fhello-kakao-friends&tbnid=5m-6mpSjEHrZLM&vet=12ahUKEwjFtYH4j-H0AhWVCt4KHb9ZAXsQMygPegUIARDxAQ..i&docid=G_LuMlbZBAamHM&w=210&h=210&itg=1&q=%EC%B9%B4%EC%B9%B4%EC%98%A4%ED%94%84%EB%A0%8C%EC%A6%88&ved=2ahUKEwjFtYH4j-H0AhWVCt4KHb9ZAXsQMygPegUIARDxAQ',
+      introduce: '안녕!!',
+      phoneNumber: '010-1234-5678',
+    },
+  ],
+
+  mypage: {
+    userEmail: 'test@test.com',
+    userName: '홍길동',
+    nickname: '스펀지밥',
+    imageUrl_profile: 'uploads/profiles/1639343163898_myPhoto.jpg',
+    introduce: '내소개',
+    phoneNumber: '010-1234-5678',
+  },
+
   cards: [
     {
       status: 200,
@@ -31,8 +75,49 @@ const addPost = createAction(ADD_POST, postInfo => ({
 const getPost = createAction(GET_POST, postInfo => ({
   postInfo,
 }));
+const getPosts = createAction(GET_POST, post_list => ({ post_list })); //항민
+const getMyPost = createAction(GET_MYPOST, myPostInfo => ({ myPostInfo })); //항민
 
 //Middleware
+const getPostDB = () => {
+  return async (dispatch, getState, { history }) => {
+    try {
+      console.log('getPostDB try!!');
+      const response = await apis.getPost();
+      console.log(response);
+
+      const post_list = response.data;
+      console.log(post_list);
+
+      const getPost = createAction(GET_POST, postInfo => ({
+        postInfo,
+      }));
+
+      dispatch(getPosts(post_list));
+    } catch (error) {
+      console.log(error);
+      window.alert(error);
+    }
+  };
+};
+
+const getMyPostDB = nickname => {
+  return async (dispatch, getState, { history }) => {
+    try {
+      console.log('getMyPostDB try!!');
+      const response = await apis.getMyPost(nickname);
+      console.log(response);
+
+      const myPostInfo = response.data;
+      console.log(myPostInfo);
+
+      dispatch(getMyPost(myPostInfo));
+    } catch (error) {
+      console.log(error);
+      window.alert(error);
+    }
+  };
+};
 
 const PostWriteFB = (content, imageUrl) => {
   return async function (dispatch, getState, { history }) {
@@ -75,11 +160,19 @@ export default handleActions(
         draft.cards.push(action.payload.postInfo);
       });
     },
-    [GET_POST]: (state, action) => {
+    [GET_ONE_POST]: (state, action) => {
       produce(state, draft => {
         draft.cards.push(action.payload.postInfo);
       });
     },
+    [GET_POST]: (state, action) =>
+      produce(state, draft => {
+        draft.postList = action.payload.post_list;
+      }),
+    [GET_MYPOST]: (state, action) =>
+      produce(state, draft => {
+        draft.myPageList = action.payload.myPostInfo;
+      }),
   },
   initialState
 );
@@ -87,6 +180,8 @@ export default handleActions(
 const actionCreators = {
   PostWriteFB,
   PostDetailLookUpFB,
+  getPostDB,
+  getMyPostDB,
 };
 
 export { actionCreators };
